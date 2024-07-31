82ab2fdbdf583bb3bb92f4984fc65268c339fede
sheikh-mamun <abalmamun2515@gmail.com>
2024-08-01 00:19:28 +0600

    new

--- practise-app/resources/views/welcome.blade.php
+++ practise-app/resources/views/welcome.blade.php
@@ -111,7 +111,7 @@
                                 </div>
 
                                 <div class="pt-3 sm:pt-5">
-                                    <h2 class="text-xl font-semibold text-black dark:text-white">my video</h2>
+                                    <h2 class="text-xl font-semibold text-black dark:text-white">shahin video</h2>
 
                                     <p class="mt-4 text-sm/relaxed">
                                         Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
@@ -122,7 +122,7 @@
                             </a>
 
                             <a
-                                href="https://laravel-news.com"
+                                href="https://www.youtube.com/"
                                 class="flex items-start gap-4 rounded-lg bg-white p-6 shadow-[0px_14px_34px_0px_rgba(0,0,0,0.08)] ring-1 ring-white/[0.05] transition duration-300 hover:text-black/70 hover:ring-black/20 focus:outline-none focus-visible:ring-[#FF2D20] lg:pb-10 dark:bg-zinc-900 dark:ring-zinc-800 dark:hover:text-white/70 dark:hover:ring-zinc-700 dark:focus-visible:ring-[#FF2D20]"
                             >
                                 <div class="flex size-12 shrink-0 items-center justify-center rounded-full bg-[#FF2D20]/10 sm:size-16">
