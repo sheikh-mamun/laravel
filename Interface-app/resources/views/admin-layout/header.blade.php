ab18b017ce0abbfabe05064df2aff53ca8ea988b
sheikh-mamun <abalmamun2515@gmail.com>
2024-08-17 18:07:28 +0600

    new

--- Interface-app/resources/views/admin-layout/header.blade.php
+++ Interface-app/resources/views/admin-layout/header.blade.php
@@ -4,7 +4,7 @@
 <div class="d-flex align-items-center justify-content-between">
   <a href="index.html" class="logo d-flex align-items-center">
     <img src="admin-assets/assets/img/logo.png" alt="">
-    <span class="d-none d-lg-block">NiceAdmin</span>
+    <span class="d-none d-lg-block">Woodland</span>
   </a>
   <i class="bi bi-list toggle-sidebar-btn"></i>
 </div><!-- End Logo -->
@@ -170,13 +170,13 @@
 
       <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
         <img src="admin-assets/assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
-        <span class="d-none d-md-block dropdown-toggle ps-2">K. Anderson</span>
+        <span class="d-none d-md-block dropdown-toggle ps-2">Sheikh mamun</span>
       </a><!-- End Profile Iamge Icon -->
 
       <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
         <li class="dropdown-header">
-          <h6>Kevin Anderson</h6>
-          <span>Web Designer</span>
+          <h6>Sheikh mamun</h6>
+          <span>Web Developer</span>
         </li>
         <li>
           <hr class="dropdown-divider">
