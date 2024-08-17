a004ab2ebeae3eb04618f7aa9a8001edacf44a5c
sheikh-mamun <abalmamun2515@gmail.com>
2024-08-17 18:22:04 +0600

    sidebar changes

--- Interface-app/resources/views/admin-layout/sidebar.blade.php
+++ Interface-app/resources/views/admin-layout/sidebar.blade.php
@@ -102,6 +102,27 @@
         </a>
       </li>
     </ul>
+
+    <!-- selling system -->
+
+    <li class="nav-item">
+    <a class="nav-link collapsed" data-bs-target="#icons-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-gem"></i><span>Selling</span><i class="bi bi-chevron-down ms-auto"></i>
+    </a>
+    <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+        <a href="icons-bootstrap.html">
+          <i class="bi bi-circle"></i><span>New Sell</span>
+        </a>
+      </li>
+      <li>
+        <a href="icons-remix.html">
+          <i class="bi bi-circle"></i><span>All Sell</span>
+        </a>
+      </li>
+    </ul>
+      
+  </li><!-- End Icons Nav -->
   </li><!-- End Icons Nav -->
 
   <li class="nav-heading">Pages</li>
@@ -139,22 +160,13 @@
       <i class="bi bi-box-arrow-in-right"></i>
       <span>Login</span>
     </a>
-  </li><!-- End Login Page Nav -->
-
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="pages-error-404.html">
-      <i class="bi bi-dash-circle"></i>
-      <span>Error 404</span>
-    </a>
-  </li><!-- End Error 404 Page Nav -->
-
-  <li class="nav-item">
-    <a class="nav-link collapsed" href="pages-blank.html">
-      <i class="bi bi-file-earmark"></i>
-      <span>Blank</span>
-    </a>
-  </li><!-- End Blank Page Nav -->
-
+  </li>
+  <!-- End Login Page Nav -->
+  
 </ul>
+</aside>
 
-</aside><!-- End Sidebar-->
+
+<!-- End Sidebar-->
+
+  
